899c8f53c7159f2bae08de94ab162fbd3418b8e7
no1tap <90048906+no1tap@users.noreply.github.com>
2023-12-02 20:16:05 +0200

    Update бот_1.py

--- бот_1.py
+++ бот_1.py
@@ -1,46 +1,62 @@
-
 import telebot
 
+import platform
 
+import subprocess
 
-
-from pythonping import ping
 import time
 
-
 bot = telebot.TeleBot('')
-response_list = ping('tststsfam.tplinkdns.com')
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, f'<b> Привіт <i>{message.from_user.first_name} {message.from_user.last_name}</i> </b>', parse_mode='html')
-@bot.message_handler(commands=['info'])
-def start(message):
-    bot.send_message(message.chat.id, f'<b> Привіт <i>{message.from_user.first_name} {message.from_user.last_name}</i> Я можу сказати чи є в тебе зараз світло вдома чи ні </b>', parse_mode='html')
-@bot.message_handler(commands=['light'])
-def start(message):
-    #bot.send_message(message.chat.id,response_list.rtt_avg_ms)
-    bot.send_message(message.chat.id,'👀Перевіряю...')
-    time.sleep(1.5)
+storage = {}
+def init_storage(user_id):
 
-    if response_list.rtt_avg_ms ==0:
-        bot.send_message(message.chat.id,f'<b>  <i>{message.from_user.first_name} {message.from_user.last_name}</i> 😟Вам не повезло у вас немає світла 😟</b>',parse_mode='html')
-    else:
-        bot.send_message(message.chat.id,f'<b>  <i>{message.from_user.first_name} {message.from_user.last_name}</i> ☺Вам повезло у вас є світло ☺</b>',parse_mode='html')
+    storage[user_id] = dict(host=None)
 
-def start(message):
-    time.sleep(300)
-    if response_list.rtt_avg_ms == 0:
-        bot.send_message(message.chat.id,f'<b>  <i>{message.from_user.first_name} {message.from_user.last_name}</i> 😟Вам не повезло у вас немає світла 😟</b>',parse_mode='html')
-@bot.message_handler()
-def start(message):
-    bot.send_message(message.chat.id, f'<b> <i>{message.from_user.first_name} {message.from_user.last_name}</i> Я розумію тіки команди. Ось вони →</b> /info інформація про мене.  /start Ви запускаєте мене  /light перевірка чи є у вас світло', parse_mode='html')
+def store_number(user_id, key, value):
+
+    storage[user_id][key] = dict(value=value)
+
+def get_number(user_id, key):
+
+    return storage[user_id][key].get('value')
+
+@bot.message_handler(commands=['start'])#початок
+def strn(message):
+
+    bot.send_message(message.chat.id, f'<b> Привіт <i>{message.from_user.first_name} {message.from_user.last_name}</i> </b>',  parse_mode = 'html')
+    time.sleep(1)
+    init_storage(message.from_user.id)
+    bot.reply_to(message, "Ваш  хост нейм?")
+    bot.register_next_step_handler(message,adress)
 
 
-    #if response_list  == 0:
-        #bot.send_message(message.chat.id('Світло немає'))
-   # if response_list != 0:
-       # bot.send_message(message.chat.id('Світло є'))
+def adress(message):
+    host = message.text
+    store_number(message.from_user.id, "host", host)
+    ip = get_number(message.from_user.id, "host")
+    time.sleep(1)
+
+    bot.send_message(message.chat.id, f"За цим хост неймом буде перевірятися світло:  {ip}" ,  parse_mode = 'html')
+
+    def ping(ip):
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
+        command = ['ping', param, '1', ip]
+        return subprocess.call(command) == 0
+    
 
 
+    status = 0
+    while True:#цикл з'ясування наявносі світла
+        kif = ping(ip)
 
-bot.polling(none_stop=True)
+        if kif == True and status == 1:
+            bot.send_message(message.chat.id,"У вас з'вилося світло")
+            status = 0
+        if kif == False and status == 0:
+            bot.send_message(message.chat.id,'У вас зникло світло')
+            status = 1
+
+        time.sleep(100)
+if __name__ == '__main__':
+    bot.skip_pending = True
+    bot.polling(none_stop=True)
